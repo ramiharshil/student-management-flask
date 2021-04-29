952ee4a0716c2f0f1524653d4860bc50fb1d9e7c
ramiharshil <77529716+ramiharshil@users.noreply.github.com>
2021-04-29 11:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -105,5 +105,5 @@ def delete_student(id):
   return student_schema.jsonify(student)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0',port=5002)
+    app.run(debug=True, host='0.0.0.0',port=5001)
 
